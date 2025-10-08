8a1004b28435d593587c1f42594e8e23e0ef0390
BLR_projs <bryanlopesrodrigues15@gmail.com>
2025-10-08 15:21:37 +0000

    aaa

--- public/router.php
+++ public/router.php
@@ -2,4 +2,15 @@
 
     define ('BASE_PATH', __DIR__ . '/../' );
 
+    switch ($ation) {
+        case 'Login':
+            
+            break;
+        case 'Cadastro':
+
+            break;
+        default:
+            # code...
+            break;
+    }
 ?>
