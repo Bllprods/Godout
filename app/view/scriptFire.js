f40b51012da81e1b6c15e1c77276af5f443856b4
Bllprods <bryanlopesrodrigues15@gmail.com>
2025-10-29 16:44:43 -0300

    a2

---
+++ app/view/scriptFire.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyDAcynqZp8KMgzcPZH2Ze5OEaFtWzMcwtk",
+    authDomain: "projhub-cc8c2.firebaseapp.com",
+    projectId: "projhub-cc8c2",
+    storageBucket: "projhub-cc8c2.firebasestorage.app",
+    messagingSenderId: "264400081700",
+    appId: "1:264400081700:web:186829ee9d7d548fd4d9b1",
+    measurementId: "G-VX0WXTYPK2"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
